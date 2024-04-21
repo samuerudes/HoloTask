9a66447199c0eea287b528a922303b36991dc2fb
samuerudes <samuellau373@gmail.com>
2024-04-21 13:57:13 +0800

    added splashscreen and updated theme

---
+++ app/src/main/java/com/itproject/holotask/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.itproject.holotask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,login.class));
+                finish();
+            }
+
+        },4000);
+    }
+}
