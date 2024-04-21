c3fc182c5d26fc9b3ae672bc3dd757388f6ee260
samuerudes <samuellau373@gmail.com>
2024-04-21 11:01:06 +0800

    added navigation menu to all necessary pages

--- app/src/main/java/com/itproject/holotask/appSettings.java
+++ app/src/main/java/com/itproject/holotask/appSettings.java
@@ -1,24 +1,31 @@
 package com.itproject.holotask;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class appSettings extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_app_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        // Setup navigation menu using existing navigationManager class
+        navigationManager.setupNavigationMenu(this, drawerLayout, navigationView, toolbar);
+
+        // Other initialization code for activity
     }
 }
